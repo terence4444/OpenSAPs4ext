85f8ad376b54424226eeb654751f74f1c20432c2
I066817 <kyle.yang@sap.com>
2018-11-27 02:00:30 +0800

    Finish: 2-2

--- application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/CreateAddressCommand.java
+++ application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/CreateAddressCommand.java
@@ -1,23 +1,33 @@
 package com.sap.cloud.s4hana.examples.addressmgr.commands;
 
+import com.netflix.hystrix.HystrixCommand;
+import com.sap.cloud.sdk.frameworks.hystrix.HystrixUtil;
+import com.sap.cloud.sdk.s4hana.connectivity.ErpCommand;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
 import org.slf4j.Logger;
 
 import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartnerAddress;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
 
-public class CreateAddressCommand {
+public class CreateAddressCommand extends ErpCommand<BusinessPartnerAddress> {
     private static final Logger logger = CloudLoggerFactory.getLogger(CreateAddressCommand.class);
 
     private final BusinessPartnerService service;
     private final BusinessPartnerAddress addressToCreate;
 
     public CreateAddressCommand(final BusinessPartnerService service, final BusinessPartnerAddress addressToCreate) {
+        super(HystrixUtil.getDefaultErpCommandSetter(
+                CreateAddressCommand.class,
+                HystrixUtil.getDefaultErpCommandProperties()
+                .withExecutionTimeoutInMilliseconds(10000)
+        ));
         this.service = service;
         this.addressToCreate = addressToCreate;
     }
 
-    public BusinessPartnerAddress execute() throws Exception {
+    @Override
+    public BusinessPartnerAddress run() throws Exception {
         return service.createBusinessPartnerAddress(addressToCreate).execute();
     }
 }
--- application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/GetAllBusinessPartnersCommand.java
+++ application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/GetAllBusinessPartnersCommand.java
@@ -1,15 +1,23 @@
 package com.sap.cloud.s4hana.examples.addressmgr.commands;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.sap.cloud.sdk.cloudplatform.cache.CacheKey;
+import com.sap.cloud.sdk.s4hana.connectivity.CachingErpCommand;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.helper.Order;
 import org.slf4j.Logger;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
 
-public class GetAllBusinessPartnersCommand {
+import javax.annotation.Nonnull;
+
+public class GetAllBusinessPartnersCommand extends CachingErpCommand<List<BusinessPartner>> {
     private static final Logger logger = CloudLoggerFactory.getLogger(GetAllBusinessPartnersCommand.class);
 
     private static final String CATEGORY_PERSON = "1";
@@ -17,10 +25,35 @@ public class GetAllBusinessPartnersCommand {
     private final BusinessPartnerService service;
 
     public GetAllBusinessPartnersCommand(final BusinessPartnerService service) {
+        super(GetAllBusinessPartnersCommand.class);
         this.service = service;
     }
 
-    public List<BusinessPartner> execute() throws Exception {
+    private static final Cache<CacheKey, List<BusinessPartner>> cache =
+            CacheBuilder.newBuilder()
+                    .maximumSize(50)
+                    .expireAfterWrite(5, TimeUnit.MINUTES)
+                    .build();
+
+    @Nonnull
+    @Override
+    protected Cache<CacheKey, List<BusinessPartner>> getCache() {
+        return cache;
+    }
+
+    @Override
+    protected CacheKey getCommandCacheKey() {
+        return CacheKey.ofTenantIsolation();
+    }
+
+    @Override
+    protected List<BusinessPartner> getFallback() {
+        logger.warn("Fallback called because of exception:" , getExecutionException());
+        return Collections.emptyList();
+    }
+
+    @Override
+    protected List<BusinessPartner> runCacheable() throws Exception {
         return service.getAllBusinessPartner()
                 .select(BusinessPartner.BUSINESS_PARTNER, BusinessPartner.LAST_NAME, BusinessPartner.FIRST_NAME)
                 .filter(BusinessPartner.BUSINESS_PARTNER_CATEGORY.eq(CATEGORY_PERSON))
